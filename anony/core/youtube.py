9f27b09872b2bfd819140a9469e23e1087e17ee4
hbbb02219-hue <hbbb02219@gmail.com>
2026-01-18 10:43:45 +0530

    Update youtube.py

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -15,8 +15,8 @@ from typing import Union
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 
 from config import API_URL, VIDEO_API_URL, API_KEY
 
